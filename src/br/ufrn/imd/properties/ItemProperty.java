4226129451fe263e2fb3dcec37ec88c61ad2c116
Inácio Medeiros <boyinacio@gmail.com>
2016-08-08 20:22:03 -0300

    implements ItemProperty

---
+++ src/br/ufrn/imd/properties/ItemProperty.java
@@ -0,0 +1,31 @@
+package br.ufrn.imd.properties;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class ItemProperty {
+	private SimpleStringProperty nome;
+	private SimpleDoubleProperty forca;
+	
+	public ItemProperty(String nome, double forca) {
+		this.nome = new SimpleStringProperty(nome);
+		this.forca = new SimpleDoubleProperty(forca);
+	}
+
+	public String getNome() {
+		return nome.get();
+	}
+
+	public void setNome(String nome) {
+		this.nome.set(nome);
+	}
+
+	public Double getForca() {
+		return forca.get();
+	}
+
+	public void setForca(Double forca) {
+		this.forca.set(forca);
+	}
+
+}
